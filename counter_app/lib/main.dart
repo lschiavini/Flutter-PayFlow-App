3267633e0839a029399f86f1da6a0f998a8fc9c2
Lucas <lucaschiavini@hotmail.com>
2021-11-06 12:32:27 -0300

    updates counter on screen

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -2,20 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.purple),
-    home: Scaffold(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Meu primeiro app"),
       ),
       body: Center(
           child: Text(
-        "CONTADOR\n0",
+        "CONTADOR\n$count",
         textAlign: TextAlign.center,
       )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+        child: const Icon(Icons.add),
       ),
-    ),
-  ));
+    );
+  }
 }
