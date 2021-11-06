c09aa0f47f1bc4826f670d47c08669c60e1eea0f
Lucas <lucaschiavini@hotmail.com>
2021-11-06 14:49:52 -0300

    Login page with social button

---
+++ payflow/lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  const SocialLoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Image.asset(AppImages.google),
+          Text(
+            "Entrar com Google",
+            style: TextStyles.buttonGray,
+          )
+        ],
+      ),
+    );
+  }
+}
