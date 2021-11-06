4ef5f7cf76566c096abdd5203ecb7b98970903a8
Lucas <lucaschiavini@hotmail.com>
2021-11-06 12:25:48 -0300

    Adding floating action button

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Meu primeiro app"),
-        ),
-        body: Container(
-            color: Colors.purple,
-            child: Center(
-                child: Container(
-              height: 200,
-              width: 200,
-              color: Colors.green,
-            ))),
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Meu primeiro app"),
+      ),
+      body: Center(
+          child: Text(
+        "CONTADOR\n0",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     ),
-  );
+  ));
 }
