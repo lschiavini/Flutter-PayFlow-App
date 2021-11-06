6baa22f762eb0a5e094deefdbe552aa4f67257b6
Lucas <lucaschiavini@hotmail.com>
2021-11-06 17:41:47 -0300

    Google sign in with firebase working

--- payflow/lib/app_widget.dart
+++ payflow/lib/app_widget.dart
@@ -17,7 +17,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
